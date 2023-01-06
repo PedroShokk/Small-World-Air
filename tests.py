2ade330bff1bce40a9f63c07a8dc353b9655db4c
PedroShokk <pribeiroeec@gmail.com>
2023-01-06 23:27:28 +0000

    close for the day -

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 import entertainment
+import alerts
 
 # Write your code below: 
 class EntertainmentSystemTests(unittest.TestCase):
@@ -20,6 +21,15 @@ class EntertainmentSystemTests(unittest.TestCase):
   def test_device_temperature(self):
     device_temp = entertainment.get_device_temp()
     self.assertLess(device_temp, 35)
+
+
+class SystemAlertTests(unittest.TestCase):
+  def test_power_outage_alert(self):
+    self.assertRaises(alerts.PowerError,alerts.power_outage_detected, True) 
+
+  def test_water_levels_warning(self):
+    self.assertWarns(alerts.WaterLevelWarning, alerts.water_levels_check, )
+
 
 
 unittest.main()
